2cf8717324263354f18c98c779a34c8972e29393
= <=>
2023-02-23 15:03:43 +0100

    SetPermissions rework

--- M365DSC/TenantExport/SetPermissions.ps1
+++ M365DSC/TenantExport/SetPermissions.ps1
@@ -1,12 +1,12 @@
 Import-Module Microsoft365DSC -Force
-$env:CONFIG = & "$PSScriptRoot\TenantConfig.ps1"
-$env:CONFIG 
-Connect-AzAccount -Tenant $env:CONFIG.TenantId
+$config = & "$PSScriptRoot\TenantConfig.ps1"
+Write-Output $config
+Connect-AzAccount -Tenant $env:TENANT_ID
 
 $permsToApply = @()
 $necessaryPermissions = @()
 
-foreach ($resource in $env:CONFIG.Resources.GetEnumerator()) {
+foreach ($resource in $config.Resources.GetEnumerator()) {
   $necessaryPermissions += $resource.Value
 }
 
@@ -21,34 +21,34 @@ foreach ($perm in $totalPermissions) {
 }
 $thumbprint = ''
 # Test if cer file is present
-if (Test-Path $env:CONFIG.CertificateCerPath) {
+if (Test-Path $config.CertificateCerPath) {
   #if so, test if is in the local storage
-  $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $env:CONFIG.CertificateCerPath
+  $cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2 $config.CertificateCerPath
   $thumbprint = $cert.Thumbprint
 
   $storeCert = Get-ChildItem 'Cert:\LocalMachine\My' | Where-Object Thumbprint -EQ $thumbprint
   if ($null -eq $storeCert) {
     Throw "A cer file is present, however the corresponding private key was not found in the local certificate store. `
-    Please import it or delete de cer file $($env:CONFIG.CertificateCerPath) and try again."
+    Please import it or delete de cer file $($config.CertificateCerPath) and try again."
   }
 }
 
 else {
   Write-Host 'No certificate found. Creating self-signed cert....'
-  $cert = New-SelfSignedCertificate -Subject "cn=$($env:CONFIG.ApplicationName)_auth" `
+  $cert = New-SelfSignedCertificate -Subject "cn=$($env:APPLICATION_NAME)_auth" `
     -CertStoreLocation 'Cert:\LocalMachine\My' `
     -NotAfter (Get-Date).AddYears(1) `
     -KeySpec KeyExchange
 
-  $cert | Export-Certificate -Type cer -FilePath $env:CONFIG.CertificateCerPath -Force 
+  $cert | Export-Certificate -Type cer -FilePath $config.CertificateCerPath -Force 
   $thumbprint = $cert.Thumbprint
 }
 
-Write-Host "Updating permissions..."
-Update-M365DSCAzureAdApplication -ApplicationName $env:CONFIG.ApplicationName `
+Write-Host 'Updating permissions...'
+Update-M365DSCAzureAdApplication -ApplicationName $env:APPLICATION_NAME `
   -Permissions $permsToApply `
   -AdminConsent `
-  -CertificatePath $env:CONFIG.CertificateCerPath `
+  -CertificatePath $config.CertificateCerPath `
   -Type Certificate
 
-$env:CONFIG.ApplicationId = (Get-AzADApplication -DisplayName $env:CONFIG.ApplicationName).AppId
+Write-Host "App ID: $((Get-AzADApplication -DisplayName $config.ApplicationName).AppId)" 
